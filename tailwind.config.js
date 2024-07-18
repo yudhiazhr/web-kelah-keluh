8470ad7156bbbd4c0a13709bbdcaa81bfd8b6502
yudhiazhr <m.yudhiazhari@gmail.com>
2024-07-18 18:40:57 +0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,13 @@ module.exports = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        'acumin-pro-bold' : ['acumin-pro-bold', 'sans-serif'],
+        'acumin-pro-book' : ['acumin-pro-book', 'sans-serif'],
+        'acumin-pro-medium' : ['acumin-pro-medium', 'sans-serif'],
+        'monbaiti' : ['monbaiti', 'sans-serif'],
+        'Garamond-Regular' : ['Garamond', 'sans-serif']
       },
     },
   },
